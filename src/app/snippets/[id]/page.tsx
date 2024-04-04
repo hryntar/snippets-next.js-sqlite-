d0f77024940c75ed0dd896c093c5cb701381eb2d
Hrynchuk Taras <hrynchuktt@gmail.com>
2024-04-04 19:04:51 +0300

    styling of snippet page

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -3,21 +3,29 @@ import { notFound } from "next/navigation";
 
 interface SnippetShowPageProps {
    params: {
-      id: string
-   }
+      id: string;
+   };
 }
 
 async function SnippetShowPage(props: SnippetShowPageProps) {
-
    const snippet = await db.snippet.findFirst({
-      where: { id: parseInt(props.params.id) }
-   })
+      where: { id: parseInt(props.params.id) },
+   });
 
    if (!snippet) return notFound();
-   
+
    return (
-      <div>
-         {snippet.title}
+      <div className="mt-5">
+         <div className="m-4 flex justify-between items-center">
+            <h1 className="text-xl font-bold">{snippet.title}</h1>
+            <div className="space-x-3">
+               <button className="p-2 border rounded">Edit</button>
+               <button className="p-2 border rounded">Delete</button>
+            </div>
+         </div>
+         <pre className="p-3 border rounded bg-gray-200 border-gray-400">
+            <code>{snippet.code}</code>
+         </pre>
       </div>
    );
 }
