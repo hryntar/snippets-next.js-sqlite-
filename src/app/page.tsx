f20c367a2cb238401a9c2d31f7df42bb8f2fb0bd
Hrynchuk Taras <hrynchuktt@gmail.com>
2024-04-04 18:55:25 +0300

    styling of home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
 import { db } from "@/db";
+import Link from "next/link";
 
 export default async function Home() {
    const snippets = await db.snippet.findMany();
 
    const renderedSnippets = snippets.map((snippet) => {
-      return <div key={snippet.id}>{snippet.title}</div>;
+      return (
+         <Link href={`/snippets/${snippet.id}`} className="flex justify-between items-center p-2 border rounded" key={snippet.id}>
+            <div>{snippet.title}</div>
+            <div>View</div>
+         </Link>
+      );
    });
 
-   return <div>{renderedSnippets}</div>;
+   return (
+      <div>
+         <div className="mt-5 flex justify-between items-center">
+            <h1 className="text-xl font-bold">Snippets</h1>
+            <Link href="snippets/new" className="p-2 border rounded">New</Link>
+         </div>
+         <div className="grid gap-3 mt-10">{renderedSnippets}</div>
+      </div>
+   );
 }
