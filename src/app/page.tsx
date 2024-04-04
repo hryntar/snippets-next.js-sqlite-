c5ec6e782f348caf70e3ca733aab9a620c637dc4
Hrynchuk Taras <hrynchuktt@gmail.com>
2024-04-04 18:23:07 +0300

    db config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { db } from "@/db";
 
+export default async function Home() {
+   const snippets = await db.snippet.findMany();
 
-export default function Home() {
-  return (
-    <div>Home page</div>
-  );
+   const renderedSnippets = snippets.map((snippet) => {
+      return <div key={snippet.id}>{snippet.title}</div>;
+   });
+
+   return <div>{renderedSnippets}</div>;
 }
