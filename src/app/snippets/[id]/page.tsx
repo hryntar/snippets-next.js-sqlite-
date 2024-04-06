115736a2b06fa1de5957a3ca662369cd7dd5226a
Hrynchuk Taras <hrynchuktt@gmail.com>
2024-04-06 18:17:06 +0300

    delete record implementation

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { db } from "@/db";
 import Link from "next/link";
 import { notFound } from "next/navigation";
+import * as actions from "@/actions";
 
 interface SnippetShowPageProps {
    params: {
@@ -11,17 +12,23 @@ interface SnippetShowPageProps {
 async function SnippetShowPage(props: SnippetShowPageProps) {
    const snippet = await db.snippet.findFirst({
       where: { id: parseInt(props.params.id) },
-   });
+   }); 
 
    if (!snippet) return notFound();
 
+   const deleteSnippetAction = actions.deleteSnippet.bind(null, snippet.id)
+   
    return (
       <div className="mt-5">
          <div className="m-4 flex justify-between items-center">
             <h1 className="text-xl font-bold">{snippet.title}</h1>
-            <div className="space-x-3">
-               <Link href={`/snippets/${snippet.id}/edit`} className="p-2 border rounded">Edit</Link>
-               <button className="p-2 border rounded">Delete</button>
+            <div className="space-x-3 flex">
+               <Link href={`/snippets/${snippet.id}/edit`} className="p-2 border rounded">
+                  Edit
+               </Link>
+               <form action={deleteSnippetAction}>
+                  <button className="p-2 border rounded">Delete</button>
+               </form>
             </div>
          </div>
          <pre className="p-3 border rounded bg-gray-200 border-gray-400">
